1ef576cc200cca9c59255c81d94e68f3a6b8ead8
V.Prasanth <prasanth.v2023@vitstudent.ac.in>
2025-01-12 23:52:13 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CareerPulse",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "CareerTrack",
+  description: "",
 };
 
 export default function RootLayout({
